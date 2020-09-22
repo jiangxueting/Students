2b73704c2be3a7d6fe0eb85069693bf1dbee9081
jiangxueting <753843448@qq.com>
2020-09-22 17:24:15 +0800

    Add the fourth student's information

--- index.php
+++ index.php
@@ -10,32 +10,56 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        
         <?php
-        include('Student.php'); $students = array();  
+        include('Student.php');
+        $students = array();  
         
         $first = new Student();
         $first->surname = "Doe";
-        $first->first_name = "John"; $first->add_email('home','john@doe.com'); $first->add_email('work','jdoe@mcdonalds.com'); $first->add_grade(65);
+        $first->first_name = "John"; $first->add_email('home','john@doe.com'); 
+        $first->add_email('work','jdoe@mcdonalds.com'); 
+        $first->add_grade(65);
         $first->add_grade(75);
         $first->add_grade(55);
         $students['j123'] = $first;
         
         $second = new Student();
         $second->surname = "Einstein";
-        $second->first_name = "Albert"; $second->add_email('home','albert@braniacs.com'); $second->add_email('work1','a_einstein@bcit.ca'); $second->add_email('work2','albert@physics.mit.edu'); $second->add_grade(95);
+        $second->first_name = "Albert"; 
+        $second->add_email('home','albert@braniacs.com'); 
+        $second->add_email('work1','a_einstein@bcit.ca'); 
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
         $second->add_grade(80);
         $second->add_grade(50);
         $students['a456'] = $second;
         
         $third = new Student();
         $third->surname = "Xueting";
-        $third->first_name = "Jiang"; $third->add_email('home','75384448@qq.com'); $third->add_email('work2','Xueting Jiang@mcdonalds.com'); $third->add_grade(80);
+        $third->first_name = "Jiang"; 
+        $third->add_email('home','75384448@qq.com'); 
+        $third->add_email('work1','Xueting Jiang@mcdonalds.com'); 
+        $third->add_grade(80);
         $third->add_grade(70);
-        $third->add_grade(50);
+        $third->add_grade(60);
         $students['A01159912'] = $third;
         
+        $fourth = new Student();
+        $fourth->surname = "huashi";
+        $fourth->first_name = "ling"; 
+        $fourth->add_email('home','12456767@qq.com'); 
+        $fourth->add_email('work1','huashiling@mcdonalds.com'); 
+        $fourth->add_grade(70);
+        $fourth->add_grade(80);
+        $fourth->add_grade(50);
+        $students['a6782'] = $fourth;
+        
+        ksort($students);
         foreach($students as $student)
         echo $student->toString();
         ?>
+
+        </table>
     </body>
 </html>
